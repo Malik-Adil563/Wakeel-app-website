5200e3348739838f9f748e2b4b7099eb59726f87
adilmalik6 <adiljobs0@gmail.com>
2025-09-02 16:23:18 +0500

    Add vercel.json for deployment

--- src/Data/FooterLinkData.jsx
+++ src/Data/FooterLinkData.jsx
@@ -1,18 +1,18 @@
 const FooterLinkData = [
   {
     id: 1,
-    title: 'Privacy Policy',
-    link: '/privacy-policy',
+    title: "Privacy Policy",
+    link: "/privacy-policy",
   },
   {
     id: 2,
-    title: 'FAQs',
-    link: '/faqs',
+    title: "FAQs",
+    link: "/faqs",
   },
   {
     id: 3,
-    title: 'Terms & Conditions',
-    link: '/terms-and-conditions',
+    title: "Terms & Conditions",
+    link: "/terms-and-conditions",
   },
 ];
 
--- src/Data/SecondaryNavbar.jsx
+++ src/Data/SecondaryNavbar.jsx
@@ -1,9 +1,9 @@
 import { Emoji } from '../Images';
 
 const SecondaryNavbar = {
-  line: 'Download the latest version of ',
-  link: 'https://play.google.com/store/apps',
-  appname: 'Wakiel-App',
+  line: "Download the latest version of ",
+  link: "https://play.google.com/store/apps",
+  appname: "Wakiel-App",
   emoji: Emoji,
 };
 
